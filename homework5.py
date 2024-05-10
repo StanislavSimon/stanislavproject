5f5e0f56a8b6fa5c754afd18dd293cae0cf3ab7e
StanislavSimon <168909475+StanislavSimon@users.noreply.github.com>
2024-05-10 11:06:37 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,19 @@
+# my_list =['apple','banana','orange','kiwi','peanapple']
+# print(my_list)
+# print(len(my_list))
+# print(my_list [0])
+# print(my_list [-1])
+# print(my_list [2:5])
+# my_list[2] = 'grape'
+# print(my_list)
+
+
+my_dict = {'apple':'яблоко','banana':'банан','orange':'апельсин','kiwi':'киви'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict['gfrape'] = 'грейпфрут'
+print(my_dict)
+my_dict.update({'tomato':'помидор','potato':'картофель','carrot':'морковь'})
+print(my_dict)
+my_dict['apple']='огурец'
+print(my_dict)
