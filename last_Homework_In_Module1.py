3a8baa72ecf545bfd2753268fc0e99e8539de94b
StanislavSimon <168909475+StanislavSimon@users.noreply.github.com>
2024-05-11 16:51:54 +0300

    Add files via upload

---
+++ last_Homework_In_Module1.py
@@ -0,0 +1,24 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(grades)
+students = sorted(students)
+print(students)
+print(len(grades[0]))
+a = grades[0]
+print(a)
+print(sum(a)/len(a))
+b = grades[1]
+print(b)
+print(sum(b)/len(b))
+c = grades[2]
+print(sum(c)/len(c))
+d = grades[3]
+print(sum(d)/len(d))
+e = grades[4]
+print(sum(e)/len(e))
+students = {'Aaron': 4.0,
+            'Bilbo': 2.25,
+            'Jonny': 4.0,
+            'Khendrik': 3.6666666666666665,
+            'Steve': 4.8}
+print(students)
