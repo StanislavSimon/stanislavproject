1bdf11b322343bd4ec149080665384dc96142388
StanislavSimon <168909475+StanislavSimon@users.noreply.github.com>
2024-06-12 15:17:59 +0300

    Add files via upload

---
+++ module 5.3.py
@@ -0,0 +1,6 @@
+class Bulding:
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
+    def __eq__(self, other):
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
