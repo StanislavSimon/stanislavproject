c44e08dad8fbbbdafc46be0649398123e640a6d9
StanislavSimon <168909475+StanislavSimon@users.noreply.github.com>
2024-09-14 12:14:03 +0300

    Add files via upload

---
+++ test_12_4_v2.py
@@ -0,0 +1,84 @@
+from dz_12_4 import Runner, Tournament
+import unittest
+import logging
+
+logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_tests.log",
+                    encoding='utf-8', format="%(asctime)s | %(levelname)s | %(message)s")
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        try:
+            runner = Runner('Test Runner ', -5)
+            for _ in range(10):
+                runner.walk()
+            self.assertEqual(runner.distance, 50)
+            logging.info('"test_walk" выполнен успешно!')
+        except TypeError as exc:
+            logging.warning("Неверная скорость для Runner", exc_info=exc)
+
+    def test_run(self):
+        try:
+            runner = Runner('Test Runner ')
+            for _ in range(10):
+                runner.run()
+            self.assertEqual(runner.distance, 100)
+            logging.info('"test_run" выполнен успешно!')
+        except TypeError as exc:
+            logging.warning('Неверный тип данных для объекта Runner', exc_info=exc)
+
+    def test_challenge(self):
+        runner_1 = Runner('Test Runner 1')
+        runner_2 = Runner('Test Runner 2')
+        for _ in range(10):
+            runner_1.walk()
+            runner_2.run()
+        self.assertNotEqual(runner_1.distance, runner_2.distance)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+class TournamentTest(unittest.TestCase):
+    if_frozen = True
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    @unittest.skipIf(if_frozen, 'Тесты в этом кейсе заморожены')
+    def setUp(self):
+        self.runner1 = Runner("Усэйн", 10)
+        self.runner2 = Runner("Андрей", 9)
+        self.runner3 = Runner("Ник", 3)
+
+    def test_usain_nick(self):
+        tournament = Tournament(90, self.runner1, self.runner3)
+        results = tournament.start()
+        TournamentTest.all_results['Усэйн, Ник'] = results
+        self.assertTrue(results[2] == 'Ник')
+
+    def test_andrey_nick(self):
+        tournament = Tournament(90, self.runner2, self.runner3)
+        results = tournament.start()
+        TournamentTest.all_results['Андрей, Ник'] = results
+        self.assertTrue(results[2] == 'Ник')
+
+    def test_usain_andrey_nick(self):
+        tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
+        results = tournament.start()
+        TournamentTest.all_results['Усэйн, Андрей, Ник'] = results
+        self.assertTrue(results[3] == 'Ник')
+
+    @classmethod
+    def tearDownClass(cls):
+        for i, elem in enumerate(cls.all_results):
+            print(f'{i + 1}: {elem}')
+
+
+if __name__ == '__main__':
+    unittest.main()
