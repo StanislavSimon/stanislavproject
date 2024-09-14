466e227d3919491d1fec54713ffd81ebd93f4de0
StanislavSimon <168909475+StanislavSimon@users.noreply.github.com>
2024-09-14 13:33:25 +0300

    Add files via upload

--- tests_12_4.py
+++ tests_12_4.py
@@ -1,59 +1,9 @@
+from dz_12_4 import Runner, Tournament
 import unittest
 import logging
 
-
-class Runner:
-    def __init__(self, name, speed=5):
-        if isinstance(name, str):
-            self.name = name
-        else:
-            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
-        self.distance = 0
-        if speed > 0:
-            self.speed = speed
-        else:
-            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
-
-    def run(self):
-        self.distance += self.speed * 2
-
-    def walk(self):
-        self.distance += self.speed
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return self.name
-
-    def __eq__(self, other):
-        if isinstance(other, str):
-            return self.name == other
-        elif isinstance(other, Runner):
-            return self.name == other.name
-
-
-class Tournament:
-    def __init__(self, distance, *participants):
-        self.full_distance = distance
-        self.participants = list(participants)
-
-    def start(self):
-        finishers = {}
-        place = 1
-        while self.participants:
-            for participant in self.participants:
-                participant.run()
-                if participant.distance >= self.full_distance:
-                    finishers[place] = participant
-                    place += 1
-                    self.participants.remove(participant)
-
-        return finishers
-
-
-logging.basicConfig(level=logging.INFO, filemode="w", filename='runner_tests.log', encoding='utf-8',
-                    format="%(asctime)s | %(levelname)s | %(message)s")
+logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_tests.log",
+                    encoding='utf-8', format="%(asctime)s | %(levelname)s | %(message)s")
 
 
 class RunnerTest(unittest.TestCase):
@@ -67,8 +17,8 @@ class RunnerTest(unittest.TestCase):
                 runner.walk()
             self.assertEqual(runner.distance, 50)
             logging.info('"test_walk" выполнен успешно!')
-        except TypeError as exc:
-            logging.warning("Неверная скорость для Runner", exc_info=exc)
+        except ValueError as exc:
+            logging.warning("Неверная скорость для Runner", exc_info=True)
 
     def test_run(self):
         try:
@@ -77,8 +27,8 @@ class RunnerTest(unittest.TestCase):
                 runner.run()
             self.assertEqual(runner.distance, 100)
             logging.info('"test_run" выполнен успешно!')
-        except TypeError as exc:
-            logging.warning('Неверный тип данных для объекта Runner', exc_info=exc)
+        except ValueError as exc:
+            logging.warning('Неверный тип данных для объекта Runner', exc_info=True)
 
     def test_challenge(self):
         runner_1 = Runner('Test Runner 1')
@@ -90,4 +40,45 @@ class RunnerTest(unittest.TestCase):
 
 
 if __name__ == "__main__":
+    unittest.main()
+
+
+class TournamentTest(unittest.TestCase):
+    if_frozen = True
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    @unittest.skipIf(if_frozen, 'Тесты в этом кейсе заморожены')
+    def setUp(self):
+        self.runner1 = Runner("Усэйн", 10)
+        self.runner2 = Runner("Андрей", 9)
+        self.runner3 = Runner("Ник", 3)
+
+    def test_usain_nick(self):
+        tournament = Tournament(90, self.runner1, self.runner3)
+        results = tournament.start()
+        TournamentTest.all_results['Усэйн, Ник'] = results
+        self.assertTrue(results[2] == 'Ник')
+
+    def test_andrey_nick(self):
+        tournament = Tournament(90, self.runner2, self.runner3)
+        results = tournament.start()
+        TournamentTest.all_results['Андрей, Ник'] = results
+        self.assertTrue(results[2] == 'Ник')
+
+    def test_usain_andrey_nick(self):
+        tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
+        results = tournament.start()
+        TournamentTest.all_results['Усэйн, Андрей, Ник'] = results
+        self.assertTrue(results[3] == 'Ник')
+
+    @classmethod
+    def tearDownClass(cls):
+        for i, elem in enumerate(cls.all_results):
+            print(f'{i + 1}: {elem}')
+
+
+if __name__ == '__main__':
     unittest.main()
