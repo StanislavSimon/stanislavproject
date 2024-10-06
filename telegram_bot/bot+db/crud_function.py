9f8be89ed2394793a18639f24dc3f281be9bf3e9
StanislavSimon <168909475+StanislavSimon@users.noreply.github.com>
2024-10-06 18:39:27 +0300

    Add files via upload

---
+++ telegram_bot/bot+db/crud_function.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+
+connection = sqlite3.connect("Products.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+id INTEGER PRIMARY KEY,
+title TEXT NOT NULL,
+description TEXT,
+price INTEGER NOT NULL
+)
+''')
+
+for i in range(1, 5):
+    cursor.execute("INSERT INTO Products (title, description, price) VALUES (?, ?, ?)", (f"Продукт{i}", f"Описание{i}",f"{i * 100}"))
+
+def get_all_products(title, description, price):
+    products = cursor.execute("SELECT * FROM Products")
+    products.fetchall()
+    return products
+
+
+
+connection.commit()
+connection.close()
